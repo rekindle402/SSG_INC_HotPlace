1ebcdd673341de065792cd0f69c0ab3b4218e2cf
rekindle <soapt@kakao.com>
2025-04-22 21:57:54 +0900

    first commit

---
+++ src/main/java/com/example/ssg_inc_hotplace/SsgIncHotPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssg_inc_hotplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgIncHotPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsgIncHotPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ssg_inc_hotplace/SsgIncHotPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssg_inc_hotplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgIncHotPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
